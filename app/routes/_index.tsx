e5d70be72b7d39b84ca78540ed8265d060ab8d05
daichi2mori <daichi2mori@gmail.com>
2024-12-20 16:25:36 +0900

    feat: Add URL validation on the client side

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,27 @@
 import type { ActionFunctionArgs } from "@remix-run/cloudflare";
 import { useFetcher } from "@remix-run/react";
 import { hc } from "hono/client";
+import { useEffect, useState } from "react";
 import type { AppType } from "server";
 import type { ActionResponse } from "types";
 import { url, pipe, safeParse, string } from "valibot";
+
+const UrlSchema = pipe(string(), url());
+
+function formatDateToJapanese(dateString: string): string {
+	const date = new Date(dateString);
+
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0");
+	const day = String(date.getDate()).padStart(2, "0");
+
+	return `${year}年${month}月${day}日`;
+}
 
 export const action = async ({ context, request }: ActionFunctionArgs) => {
 	const formData = await request.formData();
 	const urlData = formData.get("url");
 
-	const UrlSchema = pipe(string(), url());
 	const result = safeParse(UrlSchema, urlData);
 
 	if (result.success) {
@@ -51,6 +63,28 @@ export default function Index() {
 	const isLoading =
 		fetcher.state === "loading" || fetcher.state === "submitting";
 
+	const [url, setUrl] = useState("");
+	const [clientError, setClientError] = useState<string | null>(null);
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		setUrl(value);
+
+		const result = safeParse(UrlSchema, value);
+
+		if (!result.success) {
+			setClientError("有効なURLを入力してください");
+		} else {
+			setClientError(null);
+		}
+	};
+
+	useEffect(() => {
+		if (fetcher.state === "idle" && fetcher.data) {
+			setUrl(""); // 入力を空にする
+		}
+	}, [fetcher.state, fetcher.data]);
+
 	return (
 		<div>
 			<h1>Remix and Hono</h1>
@@ -61,22 +95,26 @@ export default function Index() {
 					name="url"
 					placeholder="短縮したいURLを入力"
 					className="outline"
+					value={url}
+					onChange={handleInputChange}
 				/>
-				<button type="submit" disabled={isLoading}>
+				<button type="submit" disabled={isLoading || !!clientError}>
 					送信
 				</button>
 			</fetcher.Form>
 
-			{fetcher.data?.shortUrl && (
-				<div>
-					<p>{fetcher.data.shortUrl}</p>
-					<p>{fetcher.data.expirationDate}</p>
-				</div>
-			)}
+			<div>
+				{fetcher.data?.shortUrl && <p>{fetcher.data.shortUrl}</p>}
+				{fetcher.data?.expirationDate && (
+					<p>有効期限：{formatDateToJapanese(fetcher.data.expirationDate)}</p>
+				)}
+			</div>
 
 			{fetcher.data?.error && (
 				<p className="text-red-500">{fetcher.data.error}</p>
 			)}
+
+			{clientError && <p className="text-red-500">{clientError}</p>}
 		</div>
 	);
 }
