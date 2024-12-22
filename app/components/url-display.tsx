d69edad317f2db5a2ac88bab4d6cb50fd2902dfa
daichi2mori <daichi2mori@gmail.com>
2024-12-22 19:33:35 +0900

    style: Improve UI design

--- app/components/url-display.tsx
+++ app/components/url-display.tsx
@@ -29,18 +29,18 @@ export function UrlDisplay({
 			<div className="bg-[#F8FAFC] mt-7 pt-4 px-4 pb-3 rounded md:flex md:items-center md:justify-between">
 				<p className="text-xs md:text-base text-[#757575]">{originalUrl}</p>
 				<div className="md:flex md:items-center">
-					<div className="mr-8">
-						<p className="text-base text-blue-500 mt-1" ref={copyRef}>
+					<div className="mt-1.5 md:mt-0 md:mr-8">
+						<p className="text-base text-blue-500" ref={copyRef}>
 							{shortUrl}
 						</p>
-						<p className="mt-1">
+						<p className="mt-1.5 md:mt-0">
 							有効期限：{formatDateToJapanese(expirationDate)}
 						</p>
 					</div>
 					<button
 						type="button"
 						onClick={handleCopyUrl}
-						className="bg-[#A0C238] mt-2 text-white text-xs leading-7 text-center font-mono w-full rounded md:px-5 md:text-sm md:leading-8"
+						className="bg-[#A0C238] mt-2 text-white text-xs leading-7 text-center font-mono w-full rounded md:text-sm md:leading-8 md:w-20 md:mt-0"
 					>
 						コピー
 					</button>
