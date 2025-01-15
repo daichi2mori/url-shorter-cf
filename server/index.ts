02a5fa8b3706267f80a76a2159f0dd291a3bce09
daichi2mori <daichi2mori@gmail.com>
2025-01-15 17:59:36 +0900

    feat: Add CSRF protection

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
+import { csrf } from "hono/csrf";
 import { secureHeaders } from "hono/secure-headers";
 import type { Bindings } from "types";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.use("*", secureHeaders());
+app.use("*", secureHeaders(), csrf());
 
 app.get("/:id", async (c) => {
 	const id = c.req.param("id");
